bb5df022e6ea24702e5d52637f5a7a5b36a2b7b9
mazuquiebe-filho <mazuquiebe75@gamil.com>
2023-02-01 20:07:15 -0300

    initial commit

---
+++ src/utils/userData.ts
@@ -0,0 +1,9 @@
+export const userData = {
+  nameUser: "Mazuquiebe Filho",
+  githubUser: "mazuquiebe",
+  linkedinUser: "mazuquiebe-filho-549815230",
+  whatsappNumber: "+5583987953306",
+  emailUser: "dev.mazuquiebe@gmail.com",
+  instagramUser: "dmrmazu",
+  facebookUser: "",
+};
